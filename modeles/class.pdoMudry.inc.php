99c29a606c1976bc0296e061afa2a7c63ffe08a4
Nolan2664 <johndoe@exemple.com>
2024-11-29 08:41:45 +0100

    ceci est un merge pr avoir tableau

--- modeles/class.pdoMudry.inc.php
+++ modeles/class.pdoMudry.inc.php
@@ -51,4 +51,83 @@ class PdoMudry
 		}
 		return PdoMudry::$monPdoMudry;  
 	}
+
+	
+/**
+ * Retourne tous les mouvements sous forme d'un tableau associatif
+ *
+ * @return// le tableau associatif des mouvements 
+*/
+public static function getMouvements(){
+	$req = "SELECT mouvement.Id_MOUVEMENT, mouvement.nbPlace, mouvement.numV, mouvement.distance, mouvement.heureD, mouvement.duree, mouvement.heureA, mouvement.Id_AEROPORT, mouvement.Id_AEROPORT_1, mouvement.Id_AVION FROM mouvement 
+	INNER JOIN aeroport ON mouvement.Id_AEROPORT = aeroport.Id_AEROPORT
+	INNER JOIN avion ON mouvement.Id_AVION = avion.Id_AVION";
+	
+	$res = PdoMudry::$monPdo->query($req);
+	return $res->fetchAll();
+}
+
+
+/**
+ * Retourne tous les aéroports sous forme d'un tableau associatif
+ *
+ * @return// le tableau associatif des aéroports 
+*/
+public static function getAeroports(){
+	$req = "SELECT aeroport.Id_AEROPORT, aeroport.aita, aeroport.nom, aeroport.latitude, aeroport.longitude FROM aeroport";
+
+	$res = PdoMudry::$monPdo->query($req);
+	return $res->fetchAll();
+}
+
+
+
+/**
+ * Retourne tous les modeles sous forme d'un tableau associatif
+ *
+ * @return// le tableau associatif des avions 
+*/
+    public static function getModeles(){
+        $req = "SELECT * FROM modele";
+        $res = PdoMudry::$monPdo->query($req);
+        $lesLignes = $res->fetchAll();
+		return $lesLignes;
+    }
+
+/**
+ * Retourne tous les avions sous forme d'un tableau associatif
+ *
+ * @return// le tableau associatif des avions 
+*/
+    public static function getAvions(){
+        $req = "SELECT avion.Id_AVION, avion.code, avion.numSerie, modele.Id_MODELE, modele.libelle FROM avion 
+INNER JOIN modele ON avion.Id_MODELE = modele.Id_MODELE";
+        $res = PdoMudry::$monPdo->query($req);
+        return $res->fetchAll();
+    }
+
+	function connecter($identifiant,$mdp)
+	{
+		$req = PdoMudry::$monPdo -> prepare("SELECT * FROM profil WHERE identifiant = :videntifiant");
+		$req->bindValue("videntifiant", $identifiant);
+		$req->execute();
+		foreach($req as $ligne)
+		{
+			return ($ligne['mdp']==$mdp);
+		}
+	
+		return false;
+	}
+
+	/**
+	 * affiche l'equipage
+	 */
+
+	public static function getEquipages()
+	{
+		$req = "SELECT * FROM equipage";
+		$res = PdoMudry::$monPdo ->query($req);
+		return $res->fetchAll();
+	}
+
 }
