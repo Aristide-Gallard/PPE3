fb0850d2b594489c876a013702fddafb4a7018fc
Aristide-Gallard <aristidegallard@gmail.com>
2024-12-06 10:50:11 +0100

    modif accueil header

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -1,9 +1,3 @@
 <div id="mudry">
     Ceci est un accueil
-    <a href="index.php?uc=connexion&action=seConnecter">lien connexion</a>
-    <br>
-    <a href="index.php?uc=equipage&action=voirEquipage">lien equipage</a>
-    <a href="index.php?uc=flotte&action=voirModeles">voirModeles</a>
-    <a href="index.php?uc=vol&action=gererMouvement">Gestion des vols</a>
-    <a href="index.php?uc=vol&action=gererAeroport">Gestion des aéroports</a>
 </div>
--- vues/v_header.php
+++ vues/v_header.php
@@ -5,4 +5,5 @@
     <a href="index.php?uc=vol&action=gererMouvement">Vols</a>
     <a href="index.php?uc=vol&action=gererAeroport">Aéroports</a>
     <a href="index.php?uc=equipage&action=voirEquipage">Equipage</a>
+    <a href="index.php?uc=connexion&action=seConnecter">Connexion</a>
 </header>
